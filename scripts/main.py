31fdb2bdb97ac1a6a898cb6776673c0d9fda7497
Nenetti <akinori1090@gmail.com>
2019-06-21 22:11:36 +0900

    [update] juliusとsphinxをros通信に対応させた

--- scripts/main.py
+++ scripts/main.py
@@ -18,8 +18,9 @@ import os
 class Main:
 
     def __init__(self):
-        self.julius = Julius(port=10500, config="navigation_nlp.jconf", is_debug=False)
-        self.sphinx = Sphinx(rospy.get_param("/sound_system/sphinx_config"))
+        # self.julius = Julius(port=10500, config="navigation_nlp.jconf", is_debug=False)
+
+        # self.sphinx = Sphinx(rospy.get_param("/sound_system/sphinx_config"))
         self.svox = Svox()
 
         log_file = "{}/{}".format(rospkg.RosPack().get_path("sound_system"), "logger.log")
@@ -29,6 +30,12 @@ class Main:
 
         rospy.init_node("sound_system", anonymous=False)
         rospy.Service("/sound_system/speak", StringService, self.to_speak)
+
+        # トピック名を設定
+        self.hotword_topic = "/hotword/detect"
+        self.nlp_topic = "/sound_system/nlp"
+        self.speak_topic = "/sound_system/speak"
+        self.recognition_topic = "/sound_system/recognition"
 
         # マルチスレッドで動かす
         self.thread_stop = threading.Event()
@@ -45,35 +52,23 @@ class Main:
 
     def multi_thread(self):
         while not self.thread_stop.is_set():
-            hotword_topic = "/hotword/detect"
-            nlp_topic = "/sound_system/nlp"
-            speak_topic = "/sound_system/speak"
             try:
                 # Hotword 検出処理
-                rospy.wait_for_service(hotword_topic, timeout=1)
+                rospy.wait_for_service(self.hotword_topic, timeout=1)
                 print("Hotword 待機...")
-                rospy.ServiceProxy(hotword_topic, HotwordService)()
+                rospy.ServiceProxy(self.hotword_topic, HotwordService)()
                 print("Hotword 受信")
-
-                # Juliusで音声認識処理
-                self.julius.resume()
-                text = self.julius.recognition()
-                self.julius.pause()
-
-                # Sphinxで音声認識処理
-                """
-                self.sphinx.resume()
-                text = self.julius.recognition()
-                self.sphinx.pause()
-                """
+                print("音声認識 開始")
+                rospy.wait_for_service(self.recognition_topic, timeout=1)
+                text = rospy.ServiceProxy(self.recognition_topic, StringService)().response
+                print("音声認識 終了")
                 # 認識内容のログ出力と表示
                 print(text)
                 self.output_log("julius: {}".format(text))
 
                 # 認識したテキストデータを自然言語処理に投げる
-
-                rospy.wait_for_service(nlp_topic, timeout=1)
-                nlp_result = rospy.ServiceProxy(nlp_topic, NLPService)(text)
+                rospy.wait_for_service(self.nlp_topic, timeout=1)
+                nlp_result = rospy.ServiceProxy(self.nlp_topic, NLPService)(text)
                 speak_text = nlp_result.response
 
                 # スピーク内容のログ出力と表示
@@ -81,8 +76,8 @@ class Main:
                 self.output_log("response: {}".format(speak_text))
 
                 if speak_text:
-                    rospy.wait_for_service(speak_topic, timeout=1)
-                    rospy.ServiceProxy(speak_topic, StringService)(speak_text)
+                    rospy.wait_for_service(self.speak_topic, timeout=1)
+                    rospy.ServiceProxy(self.speak_topic, StringService)(speak_text)
 
                 # 全体に投げる用
             except rospy.ROSException:
--- scripts/module/sphinx.py
+++ scripts/module/sphinx.py
@@ -7,48 +7,81 @@ from std_msgs.msg import String, Bool
 import os
 import sys
 from pocketsphinx import LiveSpeech
+from sound_system.srv import *
+import signal
 
 
 class Sphinx:
 
-    def __init__(self, file_name):
+    def __init__(self):
+
+        rospy.init_node("sphinx")
+
+        self.dict = rospy.get_param("/{}/dict".format(rospy.get_name()))
+        self.gram = rospy.get_param("/{}/gram".format(rospy.get_name()))
+
+        self.model_path = "/usr/local/lib/python2.7/dist-packages/pocketsphinx/model"  # 音響モデルのディレクトリの絶対パス
+        self.dictionary_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "dictionary")  # 辞書のディレクトリの絶対パス
         self.speech = None
-        self.model_path = '/usr/local/lib/python2.7/dist-packages/pocketsphinx/model'  # 音響モデルのディレクトリの絶対パス
-        self.dictionary_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dictionary')  # 辞書のディレクトリの絶対パス
-        self.file_name = file_name
+        self.start = False
+        self.result = None
+        self.pause()
+
+        def exit(signal, frame):
+            print("\n process exit [PID=%d]" % self.process.pid)
+            self.process.kill()
+            sys.exit(0)
+
+        signal.signal(signal.SIGINT, exit)
+
+        topic = "/sound_system/recognition"
+        rospy.Service(topic, StringService, self.recognition)
+        self.multi_thread()
+        # rospy.spin()
 
     def resume(self):
-        print('== START RECOGNITION ==')
+        print("== START RECOGNITION ==")
         self.speech = LiveSpeech(
             verbose=False, sampling_rate=8000, buffer_size=2048, no_search=False, full_utt=False,
-            hmm=os.path.join(self.model_path, 'en-us'),
+            hmm=os.path.join(self.model_path, "en-us"),
             lm=False,
-            dic=os.path.join(self.dictionary_path, "{}.dict".format(self.file_name)),
-            jsgf=os.path.join(self.dictionary_path, "{}.gram".format(self.file_name))
+            dic=os.path.join(self.dictionary_path, self.dict),
+            jsgf=os.path.join(self.dictionary_path, self.gram)
         )
 
     # 音声認識ストップ
     def pause(self):
-        print('== STOP RECOGNITION ==')
+        print("== STOP RECOGNITION ==")
         self.speech = LiveSpeech(no_search=True)
 
     # 音声認識結果の表示
-    def recognition(self):
-        for text in self.speech:
-            score = text.confidence()
-            print(text, score)
-            print(score)
-            if score > 0.1:
-                text = str(text)
-                # self.pub.publish(text)  # 音声認識の結果をpublish
-                # self.pause()
-                return text
-            else:
-                print("**noise**")
+    def recognition(self, message):
+        # type: (StringServiceRequest) -> StringServiceResponse
+        self.start = True
+        while not self.result:
+            pass
+        result = self.result
+        self.result = None
+        return StringServiceResponse(result)
+
+    def multi_thread(self):
+        while True:
+            if self.start:
+                self.resume()
+                for text in self.speech:
+                    score = text.confidence()
+                    print(str(text), score)
+                    if score > 0.1:
+                        text = str(text)
+                        # self.pub.publish(text)  # 音声認識の結果をpublish
+                        # self.pause()
+                        self.pause()
+                        self.start = False
+                        self.result = text
+                        break
+                    else:
+                        print("**noise**")
 
 
-if __name__ == '__main__':
-    sphinx = Sphinx(rospy.get_param("/sound_system/sphinx_config"))
-    sphinx.resume()
-    print("Result: {}".format(sphinx.recognition()))
-    sphinx.pause()
+if __name__ == "__main__":
+    Sphinx()
